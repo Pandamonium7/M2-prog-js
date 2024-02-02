bf7a413bce42e8068049ea4e85c2a2c64a889979
Arturo <38075@ma-web.nl>
2024-02-02 11:20:11 +0100

    veel tekenen af

--- 05veeltekenen/app.js
+++ 05veeltekenen/app.js
@@ -1,32 +1,28 @@
-class App
-{
-    runApplication()
-    {
-     //   console.log("Hello World");
+class App {
+    runApplication() {
 
-        let canvas = document.getElementById("canvasId");
-
-        let g = canvas.getContext("2d");
-        
-        this.tekenCirkel(g);
-        
-    }
-
+           this.tekenCirkel();
+    };  
 
     tekenCirkel(){
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+
+
         for (let i = 0; i < 200; i++) {
-            const x = Math.random() * canvas.width;
-            const y = Math.random() * canvas.height;
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+
 
         g.beginPath();
-        g.arc(x,y,20,0,Math.PI*2);
+        g.arc(x, y, 20, 0, Math.PI * 2);
         g.stroke();
         g.fillStyle = "red";
         g.fill();
         g.closePath();
-        }
-    }
-}
+        };
+    };
+};
 
 let app = new App();
 app.runApplication();
